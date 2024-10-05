ad1bb22f7d3216c421247071e663f818ee24b6ec
Jonah Okiru <okirujonah@gmail.com>
2024-10-05 13:38:10 +0300

    update on javaScript file

--- quiz.js
+++ quiz.js
@@ -1,18 +1,18 @@
-// the function
-function checkAnswer(){
-    // variable named correct answer
+function checkAnswer() {
+    // Identify the correct answer
     const correctAnswer = "4";
-    // select the checked radio button by its name.
-    const radio_button = document.querySelector('input[name="quiz"]:checked');
-    // the users anwer
-    const userAnswer =radio_button.value;
-    // compare user answer with the correct answer
-    if (userAnswer===correctAnswer){
-        document.getElementById('feedback').textContent = "Correct! Well done.";
-    } else if(userAnswer!==correctAnswer){
-        document.getElementById('feedback').textContent = "That's incorrect. Try again!";
-    } 
-}
-// Adding event listener to submit button
-document.getElementById("submit-answer").addEventListener('click', checkAnswer);
 
+    // Retrieve the user's answer
+    const selectedRadio = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = selectedRadio.value;
+
+    // Compare the user's answer with the correct answer
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! Well done.";
+    } else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+    }
+}
+
+// Add an event listener to the Submit Answer button
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
