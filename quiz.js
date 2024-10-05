44cd47c5f03d31470ded1624a53e750b48b96636
Jonah Okiru <okirujonah@gmail.com>
2024-10-05 13:16:16 +0300

    Task0: Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,18 @@
+// the function
+function checkAnswer(){
+    // variable named correct answer
+    const correctAnswer = "4";
+    // select the checked radio button by its name.
+    const radio_button = document.querySelector('input[name="quiz"]:checked');
+    // the users anwer
+    const userAnswer =radio_button.value;
+    // compare user answer with the correct answer
+    if(userAnswer===correctAnswer){
+        document.getElementById('feedback').textContent = "Correct! Well done.";
+    } else if(userAnswer!==correctAnswer){
+        document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+    } 
+}
+// Adding event listener to submit button
+document.getElementById("submit-answer").addEventListener('click', checkAnswer);
+
